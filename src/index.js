f9c2ede85cde76c9412ddf40f30374c18b35e5de
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-04-07 08:43:58 -0500

    se agrego webpack y babel

---
+++ src/index.js
@@ -0,0 +1,2 @@
+//usamos webpack y babel
+//npm i @babel/core babel-loader html-webpack-plugin webpack webpack-cli webpack-dev-server --save-dev
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+//usamos webpack y babel
+//npm i @babel/core babel-loader html-webpack-plugin webpack webpack-cli webpack-dev-server --save-dev
+const path = require('path');
+const htmlWebPackPluging = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebPackPluging([
+            {
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html'
+            }
+        ])
+    ]
+}
