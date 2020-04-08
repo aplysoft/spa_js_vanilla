3c0a36d267e5afa1febc9f643521379e74674295
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-04-07 23:44:18 -0500

    se agregaron las plantillas al proyecto

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-//usamos webpack y babel
-//npm i @babel/core babel-loader html-webpack-plugin webpack webpack-cli webpack-dev-server --save-dev
+console.log('hola');
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+const Home = () =>{
+    const  view = `
+    <div class="Characters">
+        <article class="Character-item">
+            <a href="#/1/">
+                <img src="" alt="">
+            </a>
+        </article>
+    </div>
+    `;
+} 
+
+module.exports = Home;
